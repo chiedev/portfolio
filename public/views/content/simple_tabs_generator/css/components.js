11abd38912aae973e29ddca860fff092be3d0b2a
chiedev <dimlaraycille@gmail.com>
2020-06-14 14:55:19 +0800

    Initial commit

--- public/views/content/simple_tabs_generator/css/components.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.switch').each(function(){
-        $('span[data-value]',this).append('<nav></nav>')
-        $('span[data-value]',this).on('click',function(){
-            (!$(this).parent().attr('checked'))?$(this).parent().attr('checked','checked'):$(this).parent().removeAttr('checked')
-        })
-    })
-});
--- public/views/content/simple_tabs_generator/js/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	generate('settings',$('#navigation'),'sidebar')
-	generate('instrument',$('#inst'),'guitar')
-	generate('settings',$('#content'),'generator-tabs')
-});
